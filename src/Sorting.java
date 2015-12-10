9a3ff414263c95b7a765fc196f112f6dfc0a6c71
BigRockerEnding <bigrockerending@live.com>
2015-12-09 18:33:02 -0800

    Worked on a new recipe

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
 
 public class Sorting {
 
+	public static void sort(String[] testArray) {
+
+		String letter;
+
+		ArrayList<String> newArray = new ArrayList<String>();
+
+		// iterates over testArray
+		for (String s : testArray) {
+
+			// iterates over newArray
+			for (int i = newArray.size() + 1; i > 0; i--) {
+
+				if (newArray.size() > 0) {
+
+					for (int j = 0; j < newArray.size(); j++) {
+						
+						letter = newArray.get(j);
+
+						if (s.compareTo(letter) <= 1) {
+
+							newArray.add(j, s);
+
+							break;
+						}
+
+					}
+				} else {
+
+					newArray.add(s);
+
+				}
+
+			}
+			
+			System.out.println(newArray);
+		}
+	}
 }
---
+++ src/ryansTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ryansTest {
+
+	@Test
+		public void testSortAnArray() throws Exception {
+		    
+			String[] testArray = { "concerned", "affected", "fascinated",
+		            	"intrigued", "absorbed", "inquisitive", "nosy", "snoopy",
+		            	"engrossed", "curious" };
+		    	Sorting.sort(testArray);
+		    	assertEquals("absorbed", testArray[0]);
+		    	assertEquals("affected", testArray[1]);
+		    	assertEquals("concerned", testArray[2]);
+		    	assertEquals("curious", testArray[3]);
+		    	assertEquals("engrossed", testArray[4]);
+		    	assertEquals("fascinated", testArray[5]);
+		    	assertEquals("snoopy", testArray[testArray.length - 1]);
+			}
+
+	/*		public void testInsertIntoArray() {
+		    	int[] testArray = { 3, 6, 8, 3, 3, 2, 98 };
+		    	testArray = Inserting.insertAt(testArray, 5, 3);
+		    	assertEquals(3, testArray[0]);
+		    	assertEquals(3, testArray[4]);
+		    	assertEquals(3, testArray[5]);
+			}
+
+			public void testInsertInOrder() throws Exception {
+		    	String[] orderedArray = { "concerned", "affected", "fascinated",
+		            	"intrigued", "absorbed", "inquisitive", "nosy", "snoopy",
+		            	"engrossed", "curious" };
+		    	Sorting.sort(orderedArray);
+
+		    	Inserting.insertAlphabetically(orderedArray, "groovy");
+		    	assertEquals("absorbed", orderedArray[0]);
+		    	assertEquals("groovy", orderedArray[6]);
+		    	assertEquals("snoopy", orderedArray[8]);
+			} */
+
+	}
+
