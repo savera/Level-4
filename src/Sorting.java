cc78ae052d047e5e31d84fa6656200ae29b32d62
Savera <Level 2>
2015-12-16 18:33:44 -0800

    worked on ryans

--- src/Sorting.java
+++ src/Sorting.java
@@ -7,17 +7,32 @@ public class Sorting {
 		String letter;
 
 		ArrayList<String> newArray = new ArrayList<String>();
+		
+		int array = newArray.size(); 
 
 		// iterates over testArray
 		for (String s : testArray) {
 
+			System.out.println(s);
+			
+			if(newArray.size() == 0){
+				
+				array = 1; 
+				
+			}
+			else{
+				
+				array = newArray.size(); 
+				
+			}
+
 			// iterates over newArray
-			for (int i = newArray.size() + 1; i > 0; i--) {
+			for (int i = array; i > 0; i--) {
 
 				if (newArray.size() > 0) {
 
 					for (int j = 0; j < newArray.size(); j++) {
-						
+
 						letter = newArray.get(j);
 
 						if (s.compareTo(letter) <= 1) {
@@ -25,6 +40,17 @@ public class Sorting {
 							newArray.add(j, s);
 
 							break;
+							
+						}
+						//if (s.compareTo(letter) > 1) 
+						else{
+
+							newArray.add(s);
+							
+							array = array--; 
+
+							//break;
+
 						}
 
 					}
@@ -32,11 +58,13 @@ public class Sorting {
 
 					newArray.add(s);
 
+					break;
+
 				}
 
+				System.out.println(newArray);
+				
 			}
-			
-			System.out.println(newArray);
 		}
 	}
 }
